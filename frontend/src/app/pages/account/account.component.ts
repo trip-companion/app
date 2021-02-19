e7cf530d02036f3569beebf17eb5d809f4045c34
Oleksii <samyner@ukr.net>
2021-02-19 14:50:34 +0200

    to do page account

--- frontend/src/app/pages/account/account.component.ts
+++ frontend/src/app/pages/account/account.component.ts
@@ -1,13 +1,17 @@
 import { DOCUMENT } from '@angular/common';
-import { ChangeDetectorRef, Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Inject, OnDestroy, OnInit } from '@angular/core';
 import { Subscription} from 'rxjs';
 
 import { Store } from '@ngrx/store';
 import { AppState } from '@app/store/app.state';
 import { GetUserAction } from '@app/store/actions/user.action';
 import { LoadGlobalEventAction } from '@app/store/actions/globalEvent.action';
-
+import { FORM_VALIDATORS } from '@app/DATA/errors-message';
 import IUserModel from '@app/interfaces/store.models/user.model';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Event } from '@angular/router';
+import { ViewChild } from '@angular/core';
+import { SharedService } from '@app/services/shared.service';
 
 @Component({
   selector: 'app-account',
@@ -15,19 +19,43 @@ import IUserModel from '@app/interfaces/store.models/user.model';
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit, OnDestroy {
+  @ViewChild('fileInput') fileInput: ElementRef;
   public user: IUserModel = null;
+  public cardHeader = '/assets/images/account/account_card_head.jpg';
+  public cardUser = '/assets/images/account/avatar-exemple.png';
+  public mainForm: FormGroup;
+  public inputErrors: string[];
   private subsUserStore: Subscription = new Subscription();
 
   constructor(@Inject(DOCUMENT) private document: Document,
-              private cdRef: ChangeDetectorRef,
-              private store: Store<AppState>) {}
+    private fb: FormBuilder,
+    private cdRef: ChangeDetectorRef,
+    private activeRoute: ActivatedRoute,
+    public sharedService: SharedService,
+    private store: Store<AppState>) {
+    this.mainForm = this.fb.group({
+      emailInput: new FormControl('', Validators.compose([Validators.email])),
+      firstNameInput: new FormControl('', Validators.required),
+      lastNameInput: new FormControl('', Validators.required),
+      passwordFirstInput: new FormControl(''),
+      passwordSecondInput: new FormControl(''),
+    }, {validators: this.checkPasswords});
+  }
 
   public ngOnInit(): void {
     this.store.dispatch(new GetUserAction());
     this.store.dispatch(new LoadGlobalEventAction());
 
+    this.activeRoute.data.subscribe(data => {
+      this.inputErrors = FORM_VALIDATORS.find(obj => data.page === obj.url)[data.lang];
+    });
+
     this.subsUserStore = this.store.select('userInfo').subscribe(({user, loading}) => {
-      if (user) { this.user = user; }
+      if (user) {
+        this.user = user;
+        this.initMainForm();
+      }
+      console.log(user, loading);
       this.cdRef.detectChanges();
     });
   }
@@ -35,5 +63,59 @@ export class AccountComponent implements OnInit, OnDestroy {
   public ngOnDestroy(): void {
     this.subsUserStore.unsubscribe();
   }
+
+  public checkPasswords(registerForm: FormGroup): any | null {
+    const firstpassword = registerForm.get('passwordFirstInput').value;
+    const confirmPassword = registerForm.get('passwordSecondInput').value;
+
+    return firstpassword === confirmPassword ? null : { notSamePassword: true };
+  }
+
+  public uploadAvatar(event: any) {
+    const imgFile = event.target as HTMLInputElement;
+    if (imgFile.files && imgFile.files[0]) {
+      const imgType = imgFile.files[0].type;
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        const image = new Image();
+        image.src = e.target.result;
+
+        image.onload = (rs: any) => {
+          const height = rs.path[0].naturalHeight;
+          const width = rs.path[0].naturalWidth;
+          if (height > 750 || width > 750) {
+            this.sharedService.setGlobalEventData(this.inputErrors[2], 'error-window');
+            this.fileInput.nativeElement.value = '';
+            return false;
+          };
+
+          const imgBase64Path = e.target.result;
+          //here method if all ok
+          console.log(imgBase64Path, imgType, this.user.email);
+        };
+
+        image.onerror = () => {
+          this.sharedService.setGlobalEventData(this.inputErrors[3], 'error-window');
+          this.fileInput.nativeElement.value = '';
+          return false;
+        };
+      };
+      reader.readAsDataURL(imgFile.files[0]);
+    } else {
+      this.sharedService.setGlobalEventData(this.inputErrors[4], 'error-window');
+    };
+  };
+
+  public onSubmitEditMainData(event: Event): void {
+
+  };
+
+
+  private initMainForm() {
+    this.mainForm.controls.firstNameInput.setValue(this.user.firstName);
+    this.mainForm.controls.lastNameInput.setValue(this.user.lastName);
+    this.mainForm.controls.emailInput.setValue(this.user.email);
+  };
 
 }
--- frontend/src/app/pages/account/account.module.ts
+++ frontend/src/app/pages/account/account.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
 import { AccountComponent } from './account.component';
 
+import { MaterialModule } from '@app/modules/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { UserEffects } from '@app/store/effects/user.effects';
@@ -13,11 +18,15 @@ import { userReducer } from '@app/store/reducers/user.reduser';
   imports: [
     CommonModule,
     AccountRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatToolbarModule,
     StoreModule.forFeature('userInfo', userReducer),
     EffectsModule.forFeature([UserEffects])
   ],
-  providers: [],
   declarations: [AccountComponent],
-  exports: [AccountComponent]
+  exports: [AccountComponent],
+  providers: [],
 })
 export class AccountModule { }
