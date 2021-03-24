ecb4a0e57c1b4f7a5d6b095948e5a867dc29f9b3
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2021-03-24 17:58:39 +0200

    Reworked translations flow

--- backend/src/main/java/com/trip/companion/rest/controller/PageItemController.java
+++ backend/src/main/java/com/trip/companion/rest/controller/PageItemController.java
@@ -1,14 +1,9 @@
 package com.trip.companion.rest.controller;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.trip.companion.domain.Language;
-import com.trip.companion.domain.page.Page;
-import com.trip.companion.domain.page.PageItem;
-import com.trip.companion.rest.dto.response.PageItemsResponse;
 import com.trip.companion.service.PageItemService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,24 +20,9 @@ public class PageItemController {
         this.service = service;
     }
 
-    @GetMapping("{pageId}/{language}")
-    public PageItemsResponse findAllByPage(@PathVariable Page pageId, @PathVariable Language language) {
-        return map(service.findAllByPageAndLanguage(pageId, language));
-    }
-
-    private PageItemsResponse map(List<PageItem> pageItems) {
-        JsonObject result = new JsonObject();
-        pageItems.forEach(pageItem -> {
-            JsonObject currentNode = result;
-            for (String nodeName : pageItem.getItem().split("\\.")) {
-                if (!currentNode.has(nodeName)) {
-                    currentNode.add(nodeName, new JsonObject());
-                }
-                currentNode = (JsonObject) currentNode.get(nodeName);
-            }
-            currentNode.addProperty("text", pageItem.getContent().getText());
-        });
-        return new PageItemsResponse(new Gson().toJson(result));
+    @GetMapping(value = "{pageId}/{language}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String findAllByPage(@PathVariable String pageId, @PathVariable Language language) {
+        return service.findAllByPageAndLanguage(pageId, language);
     }
 
 }
